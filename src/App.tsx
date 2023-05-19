17bdeea0c585f3fafa1f186ac0134bd970853ea0
Mateus Sponton <mateus.smarques.silva@gmail.com>
2023-05-19 19:34:03 -0300

    final strucutre

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>150 bits ou 10 pila no pix - bebe e acumula 1 ponto</h2>
+
+      <div className='board'>
+        <h3>2 pontos</h3>
+        <p>Analise de insta</p>
+        <p>Pergunta rapida (sim/não)</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='board'>
+        <h3>3 pontos </h3>
+        <p>Verdade ou desafio </p>
+        <p>3 minutos sem falar portugues - vc escolhe o idioma</p>
+      </div>      
+      
+      <div className='board'>
+        <h3>4 pontos </h3>
+        <p>Pergunta polemica </p>
+        <p>beija casa ou mata</p>
+      </div>      
+      
+      <div className='board'>
+        <h3>5 pontos </h3>
+        <p>karaoke </p>
+        <p>Russo maqueia</p>
+        <p>Passo um trote pra alguem</p>
+        <p>Escolha alguem pra dar TO (os mods inclusos)</p>
+      </div>      
+      
+      <div className='board'>
+        <h3>10 pontos</h3>
+        <p>Lucas da em cima de alguem na festa </p>
+      </div>      
+      
+      <div className='board'>
+        <h3>100 pontos </h3>
+        <p>Fecho a stream</p>
+        <p>Ban permanente em alguém</p>
+      </div>      
     </>
   )
 }
